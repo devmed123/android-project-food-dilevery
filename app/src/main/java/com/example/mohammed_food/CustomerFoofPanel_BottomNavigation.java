d1338dc2b0d667f29bcb66966c96d93d93c130fa
mohammed bouzit <64892812+devmed123@users.noreply.github.com>
2022-02-14 12:32:46 +0100

    first

---
+++ app/src/main/java/com/example/mohammed_food/CustomerFoofPanel_BottomNavigation.java
@@ -0,0 +1,68 @@
+package com.example.mohammed_food;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.mohammed_food.customerFoodPanel.CustomerCartFragmnet;
+import com.example.mohammed_food.customerFoodPanel.CustomerHomeFragment;
+import com.example.mohammed_food.customerFoodPanel.CustomerOrdersFragment;
+import com.example.mohammed_food.customerFoodPanel.CustomerProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class CustomerFoofPanel_BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_foof_panel_bottom_navigation);
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.cust_Home:
+                fragment=new CustomerHomeFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.cart:
+                fragment=new CustomerCartFragmnet();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.cust_profile:
+                fragment=new CustomerProfileFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.Cust_order:
+                fragment=new CustomerOrdersFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.cart:
+                fragment=new CustomerCartFragmnet();
+                break;
+        }
+        return loadcheffragment(fragment);
+
+    }
+
+    private boolean loadcheffragment(Fragment fragment) {
+
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            return true;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/mohammed_food/ReusableCodeForAll.java
@@ -0,0 +1,18 @@
+package com.example.mohammed_food;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class ReusableCodeForAll {
+    public static void ShowAlert(Context context,String title,String message){
+        AlertDialog.Builder builder=new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setPositiveButton("ok",new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int wich){
+                dialog.dismiss();
+            }
+        }).setTitle(title).setMessage(message).show();
+    }
+}
